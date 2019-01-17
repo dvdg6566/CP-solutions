5987f1146817d46f6d00d18d22d32f07f5efddbd
dvdg6566 <czhdaniel@gmail.com>
2019-01-17 21:07:25 +0800

    Added Sandy Bridge to NOIREF

--- Dunjudge/GT/cakeincake.cpp
+++ Dunjudge/GT/cakeincake.cpp
@@ -20,7 +20,7 @@ queue<pi> q,tq;
 int dx[] = {1,-1,0,0};
 int dy[] = {0,0,1,-1};
 
-int __attribute__((optimize("0fast"),target("arch=sandybridge"))) main(){
+int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   // freopen("input.txt","r",stdin);
