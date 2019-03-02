fb9f2547789b585e10868abf0f6d6770090e6655
dvdg6566 <czhdaniel@gmail.com>
2019-03-03 04:42:38 +0800

    AC_rolling

--- Dunjudge/GT/rolling.cpp
+++ Dunjudge/GT/rolling.cpp
@@ -1,56 +1,73 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+typedef pair<ll,pi> pii;
 typedef vector<pi> vpi;
+typedef set<ll> si;
 typedef long double ld;
 #define mp make_pair
 #define pb push_back
 #define f first
 #define s second
-#define MOD 1e9+7
+ll INF = 1e18;
+ll MOD = 1e9+7;
 #define lb lower_bound
 #define ub upper_bound
-#define SZ(x) (int)x.size()
+#define SZ(x) (ll)x.size()
 #define ALL(x) x.begin(),x.end()
 
-vi aList[100100];
-vpi adjList[100100];
+
+vi adjList[100100];
 int p[100100][18];
 int N,Q,a,b,root;
-int dst[100100],cnt=1;
-int low[100100];
+int depth[100100],cnt=1;
 int preorder[100100], ind[100100];
-bool empty[100100];
+bool clear[100100];
 set<int> MS;
+int low[100100];
 
-void dfs2(int x){
+void d1(int x){
+  low[x] = x;
+  for (auto i : adjList[x]){
+    d1(i);
+    low[x] = min(low[x], low[i]);
+  }
+}
+
+void dfs(int x, int u){
+  // cout<<x<<' '<<u<<'\n';
     for (auto i : adjList[x]){
-      dst[i.s] = dst[x] + 1;
-      dfs2(i.s);
+      depth[i] = depth[x] + 1;
+      dfs(i, x);
     }
     preorder[cnt] = x;
     ind[x] = cnt;
     cnt++;
 }
 
-void dfs(int x){
-  int l = x;
-  for (auto i : aList[x]){
-    dfs(i);
-    l = min(x, low[i]); 
-  } 
-  low[x] = l;
+void d2(int x){
+  for (auto i : adjList[x]){
+    p[ind[i]][0] = ind[x];
+    d2(i);
+  }
 }
 
-int find_empty(int x){
+int find_clear(int x){
+  // for (int i=1;i<=N;++i)cout<<clear[i]<<' ';cout<<'\n';
   for (int i=17;i>=0;--i){
-    if (p[x][i] != -1 && !empty[p[x][i]]){
+    if (!clear[p[x][i]]){
+      // cout<<x<<' '<<p[x][i]<<'\n';
       x = p[x][i];
     }
   }
+  // cout<<"Top "<<x<<'\n';
   return x;
+}
+
+bool cmp(int a, int b){
+  return low[a] < low[b];
 }
 
 int main(){
@@ -58,26 +75,22 @@ int main(){
   cin.tie(0);
   // freopen("input.txt","r",stdin);
   cin>>N>>Q;
-  memset(p,-1,sizeof(p));
+  memset(p,0,sizeof(p));
   for (int i=1;i<=N;++i){
     MS.insert(i);
     cin>>a;
     if (a == 0)root=i;
     else {
-      p[i][0] = a; 
-      aList[a].pb(i);
+      adjList[a].pb(i);
     }
   }
-  for (int i=1;i<=N;++i){
-    for (auto j : aList[i]){
-      adjList[i].pb(mp(low[j], j));
-    }
-  }
-  dfs(root);
-  for (int i=1;i<=N;++i)sort(ALL(adjList[i]));
-  dfs2(root);
-  for (int i=1;i<=N;++i)empty[i] = 1;
-  // for (int i=1;i<=N;++i)cout<<i<<' '<<ind[i]<<'\n';
+  d1(root);
+  // for (int i=1;i<=N;++i)cout<<low[i]<<' ';cout<<'\n';
+  for (int i=1;i<=N;++i)sort(ALL(adjList[i]), cmp);
+  for (int i=0;i<=N;++i)clear[i] = 1;
+  dfs(root, 0);
+  d2(root);
+  // for (int i=1;i<=N;++i)cout<<i<<' '<<preorder[i]<<' '<<p[i][0]<<'\n';
     for (int i = 1; i <= 17; i++){
       for (int j = 1; j <= N; j++){
         if (p[j][i-1] == -1) p[j][i] = -1;
@@ -88,21 +101,19 @@ int main(){
       cin>>a>>b;
       if (a == 1){
         int x;
-         for (int j=0;j<b;++j){
-           x = preorder[*MS.begin()];
-           MS.erase(MS.begin());
-           empty[x] = 0;
+         for (int i=0;i<b;++i){
+           x = *MS.begin();
+           MS.erase(x);
+           clear[x] = 0;
            // cout << "Fill " << x << '\n';
          } 
-         cout << x << '\n';
+         cout << preorder[x] << '\n';
       }else{
-        int x = find_empty(b);
-        cout << dst[b] - dst[x] <<'\n';
-        // assert(empty[x] == 0);
-        empty[x] = 1;
-        // cout << "Emptying " << x << '\n';
-        MS.insert(ind[x]);
-        // assert(preorder[ind[x]] == x);
+        int x = find_clear(ind[b]);
+        cout << depth[b] - depth[preorder[x]] <<'\n';
+        clear[x] = 1;
+        // cout << "clearing " << x << '\n';
+        MS.insert(x);
       }
     }
 }
