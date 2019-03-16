dd1e246571e592ba3f59612655e837924b321863
dvdg6566 <czhdaniel@gmail.com>
2019-03-16 17:18:45 +0800

    AC_arc100E

--- Dunjudge/Misc/4cuts.cpp
+++ Dunjudge/Misc/4cuts.cpp
@@ -13,55 +13,47 @@ typedef double dl;
 #define lb lower_bound
 #define ub upper_bound
 #define dst distance
-#define INF 1000000000000000000
-
+ll INF = 1e18;
+ 
 int N;
-ll A[2001000],ans=INF,a,b;
-
+ll A[201000],ans=INF,a,b,c,d,e,f,g,h;
+ 
+ll query(ll a, ll b, ll c, ll d){
+  //cout<<a<<' '<<b<<' '<<c<<' '<<d<<'\n';
+  return max({a,b,c,d}) - min({a,b,c,d});
+}
+ 
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	//freopen("input.txt","r",stdin);
 	cin>>N;
-	for(int i =1;i<=N;++i){cin>>A[i];if(i)A[i] += A[i-1];}
-	for (int i =1;i<N;++i){
+	for(int i =1;i<=N;++i){cin>>A[i];if(i)A[i] += A[i-1];} 
+	for (int i =1;i<=N;++i){
 		ll big = 0;
 		ll small = INF;
-		ll L = (A[i]+A[1])/2;
-		//cout << L << '\n';
-		auto it = lb(A,A+N,L);
+		ll L = A[i]/2;
+ 
+		auto it = ub(A+1,A+N+1,L);
 		auto jt = --it;
 		++it;
-		if (abs(*it-L) < abs(*jt-L)){
-			a = *it;
-			b = A[i] - *it;
-		}else{
-			a = *jt;
-			b = A[i] - *jt;
-		}
-		//cout << a << ' ' << b << '\n';
-		small = min(small,a);
-		big = max(big,a);
-		small = min(small,b);
-		big = max(big,b);
-		
+    a = *it;
+    b = A[i] - *it;
+    c = *jt;
+    d = A[i] - *jt;
+ 
 		L = (A[N] + A[i])/2;
-		it = lb(A,A+N,L);
+		it = ub(A+1,A+N+1,L);
 		jt = --it;
 		++it;
-		if (abs(*it-L) < abs(*jt-L)){
-			a = *it-A[i];
-			b = A[N] - *it;
-		}else{
-			a = *jt-A[i];
-			b = A[N] - *jt;
-		}
-		small = min(small,a);
-		big = max(big,a);
-		small = min(small,b);
-		big = max(big,b);
-		//cout << a << ' ' << b << '\n';
-		ans = min(ans,big-small);
+		
+		e = *it - A[i];
+    f = A[N] - *it;
+    g = *jt - A[i];
+    h = A[N] - *jt;
+ 
+    //cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<e<<' '<<f<<' '<<g<<' '<<h<<'\n';
+    ans = min({ans, query(a,b,e,f), query(a,b,g,h), query(c,d,e,f), query(c,d,g,h)});
 	}
 	cout << ans;
 }
